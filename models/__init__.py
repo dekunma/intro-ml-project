3a91bf24561a074d95ef210612ee25406b5d3097
Dekun Ma <william@dekun.me>
2022-12-03 22:29:06 -0500

    Add hrnet

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .resnet import *
 from .depth_net import *
 from .pose_resnet import *
+from .hrnet import *
--- utils/model_utils.py
+++ utils/model_utils.py
@@ -5,4 +5,5 @@ name2model = {
     'resnet152': models.resnet152,
     'depth_net': models.depth_net.DepthNet,
     'pose_resnet152': models.poseresnet152,
+    'hrnet': models.get_hrnet,
 }

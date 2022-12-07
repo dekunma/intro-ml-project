1aa9579324605475100975efc106dd9983402c58
Dekun Ma <william@dekun.me>
2022-12-07 18:05:04 -0500

    Add depth normalization to dataset

--- datasets/robot_hand.py
+++ datasets/robot_hand.py
@@ -10,9 +10,11 @@ class RobotHandDataset:
         self.split = split
         self.dataroot = dataroot
 
-        self.transforms = transforms.Compose([
+        self.transforms_rgb = transforms.Compose([
             transforms.ToTensor(), 
         ])
+
+        self.transforms_depth = self._normalize_depth_global
 
         self.field_ids = []
         self.base_path = os.path.join(self.dataroot, self.split)
@@ -21,6 +23,20 @@ class RobotHandDataset:
         self.train_size = int(self.dataset_len * 0.7)
         self.mode = mode
     
+    def _normalize_depth_global(self, depth_img):
+        depth_img = depth_img / 1000
+        min_depth = 0
+        max_depth = 65.535
+        depth_img = (depth_img - min_depth) / (max_depth - min_depth)
+        return depth_img
+
+    def _normalize_depth_per_image(self, depth_img):
+        depth_img = depth_img / 1000
+        min_depth = np.min(depth_img)
+        max_depth = np.max(depth_img)
+        depth_img = (depth_img - min_depth) / (max_depth - min_depth)
+        return depth_img
+
     def __getitem__(self, idx):
         if self.mode == 'tail':
             idx += self.train_size
@@ -31,11 +47,11 @@ class RobotHandDataset:
 
         for ii in [0,1,2]:
             rgb_img = Image.open(os.path.join(base_path_x, f'rgb/{ii}.png'))
-            rgb_img = self.transforms(rgb_img)
+            rgb_img = self.transforms_rgb(rgb_img)
 
             depth_img = np.load(os.path.join(base_path_x, 'depth.npy'))[ii]
-            depth_img = Image.fromarray(depth_img)
-            depth_img = self.transforms(depth_img)
+            depth_img = self.transforms_depth(depth_img)
+            depth_img = torch.from_numpy(depth_img).unsqueeze(0)
             if image_input == None:
                 image_input = torch.cat((rgb_img, depth_img), dim=0)
             else:

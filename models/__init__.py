c5fa568735bcae7d18b40429e42f6b9e231b5eb3
Dekun Ma <william@dekun.me>
2022-12-08 21:39:19 -0500

    Add convnext implementation

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from .depth_net import *
 from .pose_resnet import *
 from .hrnet import *
 from .resnest import *
+from .convnext import *
--- utils/utils.py
+++ utils/utils.py
@@ -19,6 +19,7 @@ def get_model(model_name):
         'pose_resnet152': models.poseresnet152,
         'hrnet': models.get_hrnet,
         'resnest269': models.resnest.resnest269,
+        'convnext': models.convnext.convnext_xlarge,
     }
 
     if model_name not in name2model:
